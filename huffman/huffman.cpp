4a11840c93413975647687f634d2527588570726
stefanos-baziotis <sdi1600105@di.uoa.gr>
2019-02-17 16:08:48 +0200

    huffman: First complete draft version

--- huffman/huffman.cpp
+++ huffman/huffman.cpp
@@ -1,18 +1,60 @@
+#include <assert.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+
+void print_bin(uint32_t v, int lim) {
+    uint32_t r = 1U << 31;
+    while(r && lim--) {
+        if(v & r) {
+            printf("1");
+        } else {
+            printf("0");
+        }
+        r >>= 1;
+    }
+    printf("\n");
+}
 
 // The basic data structure for Huffman encoding is a min heap.
 
+typedef struct symbol symbol_t;
+
+typedef struct symbol {
+public:
+    char *sym;
+    int freq;
+    symbol_t *left, *right;
+
+    static int cmp(symbol_t *a, symbol_t *b) {
+        return (a->freq - b->freq);
+    }
+
+    symbol_t& operator=(symbol_t const& copy) {
+        sym = copy.sym;
+        freq = copy.freq;
+        left = copy.left;
+        right = copy.right;
+        return *this;
+    }
+} symbol_t;
+
 typedef struct {
 private:
-    int *data;
+    symbol_t *data;
     int capacity;
     int used;
+    int (*symbol_cmp)(symbol_t *, symbol_t *);
 
     void swap(int a, int b) {
-        int temp = data[a];
+        symbol_t temp = data[a];
         data[a] = data[b];
         data[b] = temp;
+    }
+
+    int cmp(int a, int b) {
+        return symbol_cmp(&data[a], &data[b]);
     }
 
 // offsets start from 0
@@ -23,7 +65,7 @@ private:
     void heapify_up(int index) {
         while(index != 0) {
             int p = parent(index);
-            if(data[p] > data[index]) {
+            if(cmp(p, index) > 0) {
                 swap(p, index);
                 index = p;
             } else {
@@ -48,10 +90,10 @@ private:
         // if there's no left child, then certainly there's no right child.
         while(l = has_left(index)) {
             int smallest = l;
-            if((r = has_right(index)) && data[r] < data[smallest]) {
+            if((r = has_right(index)) && cmp(r, smallest) < 0) {
                 smallest = r;
             }
-            if(data[index] < data[smallest]) {
+            if(cmp(index, smallest) < 0) {
                 break;
             } else {
                 swap(index, smallest);
@@ -61,26 +103,29 @@ private:
     }
 
 public:
-    void initialize(int capacity) {
-        data = (int *) malloc(capacity * sizeof(int));
-        capacity = capacity;
+    void initialize(int cap, int (*sym_cmp)(symbol_t *a, symbol_t *b)) {
+        data = (symbol_t *) malloc(cap * sizeof(symbol_t));
+        capacity = cap;
         used = 0;
+        symbol_cmp = sym_cmp;
     }
 
-    void insert(int v) {
+    void insert(symbol_t s) {
         // insert into a new leaf node
-        data[used] = v;
+        data[used] = s;
         // O(logn) heapify up
         heapify_up(used);
         ++used;
     }
 
-    int remove_min(void) {
+    symbol_t remove_min(void) {
+        symbol_t res;
+        res.freq = -1;
         // IMPORTANT(stefanos): value on empty heap depends on
         // usage.
-        if(!used) return -1;
+        if(!used) return res;
 
-        int res = data[0];
+        res = data[0];
         swap(0, used-1);
         --used;
         // O(logn) heapify down
@@ -90,47 +135,78 @@ public:
 
     void print(void) {
         for(int i = 0; i != used; ++i) {
-            printf("%d ", data[i]);
+            printf("%d ", data[i].freq);
         }
         printf("\n");
+    }
+
+    void print_vlc(symbol_t *root, uint32_t v, int index) {
+        if(root != NULL) {
+            if(root->left == NULL && root->right == NULL) {  // leaf
+                printf("%s: ", root->sym);
+                print_bin(v, index);
+            }
+            print_vlc(root->left, v, index+1);
+            v = v | (1U << (31 - index));
+            print_vlc(root->right, v, index+1);
+        }
+    }
+
+    void build_vlc(void) {
+        symbol_t *syms = (symbol_t *) malloc(3 * capacity * sizeof(symbol_t));
+        assert(syms != NULL);
+        int i = 0;
+        while(used > 1) {
+            symbol_t a = remove_min();
+            syms[i] = a;
+            symbol_t *ap = &syms[i];
+            i++;
+            symbol_t b = remove_min();
+            syms[i] = b;
+            printf("a: %d, b: %d\n", a.freq, b.freq);
+            symbol_t *bp = &syms[i];
+            i++;
+            symbol_t *cp = &syms[i];
+            cp->freq = a.freq + b.freq;
+            cp->left = bp;
+            cp->right = ap;
+            insert(syms[i]);
+            i++;
+            print();
+        }
+        symbol_t root = remove_min();
+        print_vlc(&root, 0U, 0);
+        free(syms);
     }
 
     void free_heap(void) {
         free(data);
     }
-} min_heap_t;
+} huffman_heap_t;
 
 int main(void) {
-    min_heap_t min_heap;
-    int min;
+    huffman_heap_t huffman_heap;
+    symbol_t min;
+    symbol_t temp;
+    temp.left = NULL;
+    temp.right = NULL;
 
-    min_heap.initialize(8);
-    min_heap.insert(45);
-    min_heap.print();
-    min_heap.insert(3);
-    min_heap.print();
-    min_heap.insert(7);
-    min_heap.print();
-    min_heap.insert(1);
-    min_heap.print();
-    min_heap.insert(9);
-    min_heap.print();
+    huffman_heap.initialize(3, symbol_t::cmp);
+    temp.sym = strdup("A");
+    temp.freq = 4;
+    huffman_heap.insert(temp);
+    huffman_heap.print();
+    temp.sym = strdup("B");
+    temp.freq = 1;
+    huffman_heap.insert(temp);
+    huffman_heap.print();
+    temp.sym = strdup("C");
+    temp.freq = 1;
+    huffman_heap.insert(temp);
+    huffman_heap.print();
 
-    min = min_heap.remove_min();
-    printf("min: %d\n", min);
-    min_heap.print();
-    min = min_heap.remove_min();
-    printf("min: %d\n", min);
-    min_heap.print();
-    min = min_heap.remove_min();
-    printf("min: %d\n", min);
-    min_heap.print();
-    min = min_heap.remove_min();
-    printf("min: %d\n", min);
-    min_heap.print();
-    min = min_heap.remove_min();
-    printf("min: %d\n", min);
-    min_heap.print();
-    min_heap.free_heap();
+    printf("\nBuild VLC\n");
+    huffman_heap.build_vlc();
+    huffman_heap.free_heap();
     return 0;
 }
